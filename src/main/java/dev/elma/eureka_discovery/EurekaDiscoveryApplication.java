e90a07bdc329ef0d816e2dd578e883854d45aec9
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-25 23:43:24 +0100

    commit

--- src/main/java/dev/elma/eureka_discovery/EurekaDiscoveryApplication.java
+++ src/main/java/dev/elma/eureka_discovery/EurekaDiscoveryApplication.java
@@ -3,9 +3,10 @@ package dev.elma.eureka_discovery;
 import com.netflix.discovery.EurekaNamespace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaSer
+@EnableEurekaServer
 public class EurekaDiscoveryApplication {
 
     public static void main(String[] args) {
